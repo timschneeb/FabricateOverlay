8a1fe6ae04ce7c0e4027fa271bd0d632e911ebb5
Tim Schneeberger <tim.schneeberger@outlook.de>
2026-02-12 06:16:37 +0100

    Add more prefixes

--- app/src/main/java/tk/zwander/fabricateoverlaysample/ui/fragments/FilterBottomSheetFragment.kt
+++ app/src/main/java/tk/zwander/fabricateoverlaysample/ui/fragments/FilterBottomSheetFragment.kt
@@ -87,6 +87,8 @@ class FilterBottomSheetFragment : BottomSheetDialogFragment() {
             LEGACY_SUPPORT("Legacy Support library", listOf("design_")),
             MATERIAL_COMPONENTS("Material", listOf("m3_", "mtrl_", "material_")),
             SAMSUNG_EXTENDED_SUPPORT("Samsung Extended Support library", listOf("sesl_")),
+            SETTINGS_LIB("SettingsLib", listOf("settingslib_")),
+            SETUP_WIZARD_LIB("SetupWizardLib", listOf("sud_")),
             OTHER_3RD_PARTY_LIBS("3rd-party libraries", listOf(
                 "exo_", "apploving_", "glide_", "com_facebook_", "common_google_signin_", "mbridge_",
                 "ad_mob_", "com_braze_", "secmtp_", "ia_", "uxc_"
